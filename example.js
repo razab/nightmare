207a90443f70a106bd3697e52e1259dd8bafc247
Denis Coroliov <denis.coroliov@gmail.com>
2016-02-26 14:52:57 +0200

    adding webContents.sendInputEvent

--- example.js
+++ example.js
@@ -4,7 +4,8 @@ var nightmare = Nightmare({ show: true })
 nightmare
   .goto('http://yahoo.com')
   .type('input[title="Search"]', 'github nightmare')
-  .click('#UHSearchWeb')
+  // .click('#UHSearchWeb')
+  .click('#uh-search-button')
   .wait('#main')
   .evaluate(function () {
     return document.querySelector('#main .searchCenterMiddle li a').href
